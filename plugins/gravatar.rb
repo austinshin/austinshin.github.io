ae74dfe5058fc19e6f96193908fcf2d186073fb9
Austin Shin <shinaustin@gmail.com>
2015-12-21 03:28:44 -0800

    changing statusbar

--- plugins/gravatar.rb
+++ plugins/gravatar.rb
@@ -1,42 +1,22 @@
-# include the MD5 gem, this should be part of a standard ruby install
 require 'digest/md5'
 
 module Jekyll
+  module GravatarFilter
 
-  class Gravatar < Liquid::Tag
-
-    def initialize(tag_name, size, token)
-      super
-      @size = size.strip
+    # Add our new liquid filter.
+    def get_gravatar(input)
+      "//www.gravatar.com/avatar/#{hash(input)}"
     end
 
-    def render(context)
-      # get the site config variables
-      site_config = context.registers[:site].config
+    private :hash
 
-      # get the email address from the site config
-      email_address = site_config['gravatar_email']
-      # change the email address to all lowercase
-      email_address = email_address.downcase
-
-      # create an md5 hash from the email address
-      gravatar_hash = Digest::MD5.hexdigest(email_address)
-
-      # compile the full Gravatar URL
-      image_src = "http://www.gravatar.com/avatar/#{gravatar_hash}"
-
-      # append size query to URL if provided in tag
-      unless @size.empty?
-        image_src = image_src+"?s=#{@size}"
-      end
-
-      # output the full Gravatar URL
-      image_src
+    # Clean up the email address and return hashed version.
+    def hash(email)
+      email_address = email ? email.downcase.strip : ''
+      Digest::MD5.hexdigest(email_address)
     end
-
   end
-
 end
 
-# register the "gravatar_image" tag
-Liquid::Template.register_tag('gravatar_image', Jekyll::Gravatar)
+Liquid::Template.register_filter(Jekyll::GravatarFilter)
+
